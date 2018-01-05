13ac3da9d8d37de3f81bc9deba60f6de76649ebb
anis.tajouri <anis.tajouri@gmail.com>
2018-01-05 16:42:20 +0100

    First commit
    Signed-off-by: anis.tajouri <anis.tajouri@gmail.com>

---
+++ webapi/views/SystemDateView.py
@@ -0,0 +1,15 @@
+from time import strftime
+
+from rest_framework.permissions import AllowAny
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+
+class SystemDateList(APIView):
+    permission_classes = (AllowAny,)
+
+    def get(self, request, format=None):
+        # get the local system date
+        clock = strftime("%Y-%m-%dT%H:%M:%S")
+
+        return Response(str(clock))
